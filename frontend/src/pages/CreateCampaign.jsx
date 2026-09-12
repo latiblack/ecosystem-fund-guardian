ecebfadb759a6e2fff0aa5872c7e4550a686031b
latiblack <latiblack@github.com>
2026-09-13 07:43:36 +0800

    cleanup: remove unused useEffect import

--- frontend/src/pages/CreateCampaign.jsx
+++ frontend/src/pages/CreateCampaign.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useSignMessage } from "wagmi";
 import { useWallet } from "../context/WalletContext";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
